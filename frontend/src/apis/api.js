3d58c08391242192edb9705591dc0eaacdd4888f
HyunSense <hyunsense1022@gmail.com>
2025-01-10 02:42:41 +0900

    일부 주석 변경

--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
+  // aws EC2 Domain
   baseURL: "https://api.withdog.store/api/v1",
   // baseURL: "http://localhost:8080/api/v1",
   headers: {
